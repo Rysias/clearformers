d199181360a668c123daba88da984ca971e26a77
rysias <jvhithamar@gmail.com>
2022-01-04 17:10:52 +0100

    remove unused files

--- setup.py
+++ setup.py
@@ -8,12 +8,7 @@ setuptools.setup(
     description="Creating explainable embeddings",
     url="https://github.com/Rysias/clearformers/",
     author="Jonathan Rystroem",
-    install_requires=[
-        "scikit-learn==1.0.1",
-        "numpy>=1.20.0",
-        "bertopic>=0.9.3",
-        "matplotlib",
-    ],
+    install_requires=["scikit-learn==1.0.1", "numpy>=1.20.0", "bertopic>=0.9.3",],
     extras_requires={"dev": ["pytest", "python-semantic-release"]},
     author_email="jhvithamar@gmail.com",
     packages=setuptools.find_packages(),
